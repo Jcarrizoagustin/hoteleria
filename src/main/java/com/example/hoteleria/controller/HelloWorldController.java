4919382f4d936e50549653cd2e89d14430e154ba
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-02-07 14:49:17 -0300

    primer commit

---
+++ src/main/java/com/example/hoteleria/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.hoteleria.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world!!";
+    }
+}
